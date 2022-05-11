95168fbd79683396de37333bca0c5941ebf2bbc8
galustoff <galustoff.dan@yandex.ru>
2022-05-11 08:29:55 +0300

    set cursor pipette

--- src/AppRandomizerGrid.vue
+++ src/AppRandomizerGrid.vue
@@ -8,24 +8,25 @@
 </template>
 
 <script>
-import AppRandomizerGridField from './AppRandomizerGridField.vue'
+  import AppRandomizerGridField from './AppRandomizerGridField.vue'
 
-export default {
-  props: ['colorFlag'],
-  components: {
-    AppRandomizerGridField
+  export default {
+    props: ['colorFlag'],
+    components: {
+      AppRandomizerGridField
+    }
   }
-}
 </script>
 
 <style>
-.app-randomizer__grid {
-  font-size: 0;
-  list-style: none;
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 1);
-  overflow: hidden;
-  border-radius: 10px;
-}
+  .app-randomizer__grid {
+    font-size: 0;
+    list-style: none;
+    width: 400px;
+    height: 400px;
+    background-color: rgba(255, 255, 255, 1);
+    overflow: hidden;
+    border-radius: 10px;
+    cursor: url('~assets/pipette.svg') 0 24, pointer;
+  }
 </style>
